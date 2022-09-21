e76e543117ad329208665f87e150e3882aaf0753
elcharitas <jonathanirhodia@gmail.com>
2022-09-21 07:28:32 +0100

    feat: create homepage content

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { IPage } from "src/types";
+import { HeroContent } from "src/sections";
+
+const Page: FC<IPage> = ({}) => {
+    return (
+        <>
+            <HeroContent />
+        </>
+    );
+};
+
+Page.displayName = "Welcome";
+
+export default Page;
